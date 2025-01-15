1c216c17af42aeec4838ef67489d50f05b9437a1
nnasttia <khaia.nastia@gmail.com>
2025-01-15 13:25:05 +0200

    added contact page

---
+++ src/pages/AppContactsPage.vue
@@ -0,0 +1,183 @@
+<template>
+  <main class="container d-flex gap-45">
+    <div class="contacts ">
+      <h2 class="contacts__title">Write to us with <br> any questions.</h2>
+      <div class="contacts__content d-flex">
+        <div class="contacts__stores d-flex">
+          <div class="store">
+            <h3 class="store__title">Berkley store</h3>
+            <address class="store__address">
+              2927 Newbury St Suite A,<br />
+              Berkeley,<br />
+              CA 94703,<br />
+              USA
+            </address>
+          </div>
+          <div class="store">
+            <h3 class="store__title">Montreal store</h3>
+            <address class="store__address">
+              2001 Blvd. Robert Bourassa,<br />
+              Suite 1700,<br />
+              Montreal,<br />
+              Quebec,<br />
+              H3A 2A6,<br />
+              Canada
+            </address>
+          </div>
+        </div>
+      </div>
+      <div class="social-media d-flex">
+        <a href="#" target="_blank" title="Facebook">
+          <span class="screen-reader-only">Facebook social media</span>
+          <svg width="24" height="24" viewBox="0 0 310 310" fill="#1e6648"><path d="M81.703 165.106h33.981V305a5 5 0 0 0 5 5H178.3a5 5 0 0 0 5-5V165.765h39.064a5 5 0 0 0 4.967-4.429l5.933-51.502a5 5 0 0 0-4.966-5.572h-44.996V71.978c0-9.732 5.24-14.667 15.576-14.667h29.42a5 5 0 0 0 5-5V5.037a5 5 0 0 0-5-5h-40.545A39.746 39.746 0 0 0 185.896 0c-7.035 0-31.488 1.381-50.804 19.151-21.402 19.692-18.427 43.27-17.716 47.358v37.752H81.703a5 5 0 0 0-5 5v50.844a5 5 0 0 0 5 5.001z"/></svg>
+        </a>
+        <a href="#" target="_blank" title="X">
+          <span class="screen-reader-only">X social media</span>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="#1e6648"><path d="m2.867 3 6.87 9.818L2.733 21h2.647l5.539-6.49L15.46 21h5.911l-7.197-10.303L20.744 3H18.14l-5.143 6.01L8.8 3H2.867z"/></svg>
+        </a>
+        <a href="#" target="_blank" title="YouTube">
+          <span class="screen-reader-only">YouTube social media</span>
+          <svg width="24" height="24" viewBox="0 -3 20 20" fill="#1e6648"><path d="M7.988 9.586V3.974c1.993.938 3.536 1.843 5.36 2.82-1.505.834-3.367 1.77-5.36 2.792m11.103-8.403c-.344-.453-.93-.805-1.553-.922-1.833-.348-13.267-.349-15.099 0-.5.094-.945.32-1.328.673C-.5 2.429.005 10.452.393 11.75c.164.562.375.968.64 1.235.343.352.812.594 1.351.703 1.51.312 9.284.486 15.122.047a2.62 2.62 0 0 0 1.39-.712c1.49-1.49 1.388-9.962.195-11.841"/></svg>
+        </a>
+        <a href="#" target="_blank" title="Instagram">
+          <span class="screen-reader-only">Instagram social media</span>
+          <svg width="24" height="24" viewBox="0 0 20 20" fill="#1e6648"><path d="M5.87.123C4.242.196 2.83.594 1.69 1.729.548 2.869.155 4.286.081 5.897.037 6.902-.231 14.498.545 16.49a5.04 5.04 0 0 0 2.91 2.903c.634.246 1.356.412 2.416.461 8.86.401 12.145.183 13.53-3.364.246-.631.415-1.353.462-2.41.405-8.883-.066-10.809-1.61-12.351C17.027.507 15.586-.325 5.87.123m.081 17.944c-.97-.043-1.496-.205-1.848-.341a3.255 3.255 0 0 1-1.888-1.883c-.591-1.514-.395-8.703-.342-9.866.051-1.14.282-2.18 1.086-2.985C3.954 2 5.24 1.513 13.993 1.908c1.142.052 2.186.282 2.992 1.084.995.993 1.489 2.288 1.087 11.008-.044.968-.206 1.493-.342 1.843-.901 2.308-2.973 2.628-11.779 2.224M14.09 4.69c0 .657.534 1.19 1.194 1.19.66 0 1.195-.533 1.195-1.19a1.194 1.194 0 0 0-2.39 0M4.864 9.988a5.103 5.103 0 0 0 5.11 5.097 5.103 5.103 0 0 0 5.109-5.097 5.102 5.102 0 0 0-5.11-5.096 5.102 5.102 0 0 0-5.11 5.096m1.794 0A3.313 3.313 0 0 1 9.972 6.68a3.313 3.313 0 0 1 3.317 3.308 3.313 3.313 0 0 1-3.317 3.31 3.313 3.313 0 0 1-3.316-3.31"/></svg>
+        </a>
+        <a href="#" target="_blank" title="Pinterest">
+          <span class="screen-reader-only">Pinterest social media</span>
+          <svg width="24" height="24" viewBox="0 0 20 20" fill="#1e6648"><path d="M9.876 0C4.399 0 0 4.435 0 9.959c0 4.1 2.407 7.53 5.893 9.12 0-1.867-.013-1.626 1.41-7.699-.782-1.578-.174-4.183 1.577-4.183 2.43 0 .779 3.603.498 5.02-.249 1.088.581 1.925 1.577 1.925 1.909 0 3.153-2.427 3.153-5.271 0-2.176-1.493-3.767-4.066-3.767-4.575 0-5.735 5.096-4.149 6.695.399.604 0 .632 0 1.423-.277.836-2.49-.38-2.49-3.514 0-2.846 2.324-6.193 7.054-6.193 3.734 0 6.224 2.761 6.224 5.69 0 3.933-2.158 6.779-5.311 6.779-1.079 0-2.075-.586-2.407-1.256-.603 2.346-.725 3.535-1.66 4.854.913.25 1.826.418 2.822.418C15.602 20 20 15.565 20 10.042 19.752 4.435 15.353 0 9.876 0"/></svg>
+        </a>
+      </div>
+    </div>
+    <form class="contacts__form">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" placeholder="Type your name" />
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" placeholder="Type your email" />
+      </div>
+      <div class="form-group">
+        <label for="message">Message:</label>
+        <textarea id="message" placeholder="Type your message"></textarea>
+      </div>
+      <button class="btn form-group-button">Send message</button>
+    </form>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.contacts {
+  margin-top: 30px;
+  padding: 20px;
+
+  &__title {
+    font-size: 48px;
+    color: var(--second-color);
+    font-family: var(--second-font),serif;
+    margin-bottom: 46px;
+    font-weight: 400;
+    text-transform: none;
+    line-height: 64px;
+    letter-spacing: 0;
+  }
+
+  &__content {
+    gap: 20px;
+  }
+
+  &__stores {
+    flex: 1;
+    gap: 100px;
+
+    .store {
+
+      &__title {
+        font-size: 24px;
+        font-weight: 400;
+        color: var(--second-color);
+        font-family: var(--second-font),serif;
+        margin-bottom: 10px;
+        line-height: 34px;
+        letter-spacing: 0;
+      }
+
+      &__address {
+        font-size: 18px;
+        line-height: 28px;
+        font-family: var(--main-font),serif;
+        color: #1f1f1f;
+        font-weight: 400;
+        letter-spacing: 0;
+      }
+      margin-bottom: 56px;
+    }
+  }
+
+  .social-media {
+
+    a {
+      margin-right: 14px;
+      background-color: rgba(0, 0, 0, 0);
+      font-size: 24px;
+      padding: .5em;
+    }
+
+    svg:hover {
+      fill: #6d2a2a;
+    }
+  }
+
+  &__form {
+    flex: 1;
+    margin-top: 65px;
+
+    .form-group {
+      margin-bottom: 15px;
+
+      label {
+        display: block;
+        margin-bottom: 5px;
+        color: #a5a5a5;
+        font-size: 18px;
+        font-weight: 400;
+      }
+
+      input,
+      textarea {
+        width: 100%;
+        font-size: 18px;
+        font-family: var(--main-font), serif;
+        font-weight: 400;
+        letter-spacing: 0;
+        padding: 16px 0 17px 30px;
+        margin: 7px 0 3px 0;
+        border-radius: 0;
+        background-color: #f4f6f5;
+        border-style: solid;
+        border-width: 0;
+      }
+
+      textarea {
+        resize: vertical;
+        min-height: 241px;
+      }
+
+    }
+    .form-group-button {
+      display: block;
+      width: 100%;
+
+      &:hover,
+      &:active,
+      &:focus {
+        background-color: var(--second-color);
+      }
+    }
+  }
+}
+</style>
